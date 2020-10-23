056e1a922207d3bc76aeac38ab703228857bc869
Dips <46165604+idohabarvaz@users.noreply.github.com>
2020-10-24 00:06:21 +0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -295,7 +295,6 @@ async def host(ctx, players: int, *, role:discord.Role=None):
 
                 await msg15.remove_reaction("📝", host)
 
-                await txt.send("Type The Name You Wish To Change To")
 
                 await txt.set_permissions(host, send_messages=True, read_messages=True)
                 lobbyName = await bot.wait_for("message",check=nameCheck)
